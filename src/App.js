6a88e7caa4935ef6b393a7f5a50afa5e6c22d032
Barry Hanna <barryhanna@users.noreply.github.com>
2021-11-24 10:53:42 +0000

    added intro page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IntroPage = () => (
+  <div className="intro flow">
+    <h1>Quizzical</h1>
+    <p>Some description is needed</p>
+    <button>Start quiz</button>
+  </div>
+);
+
+export default IntroPage;
